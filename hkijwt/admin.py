b02cccd01dc4a3418465e55439e5bb7279e92553
Juha Yrjölä <juha.yrjola@iki.fi>
2015-10-31 15:26:49 +0200

    Add admin for AppToAppPermission

--- hkijwt/admin.py
+++ hkijwt/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import AppToAppPermission
 
-# Register your models here.
+
+class AppToAppPermissionAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(AppToAppPermission, AppToAppPermissionAdmin)
