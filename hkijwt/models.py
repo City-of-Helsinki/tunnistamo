71695f6cb8f939de924c29ef5ba2d69326608fa1
Juha Yrjölä <juha.yrjola@iki.fi>
2016-02-29 19:09:55 +0200

    Add __str__ method to AppToAppPermission

--- hkijwt/models.py
+++ hkijwt/models.py
@@ -7,3 +7,6 @@ class AppToAppPermission(models.Model):
                                   db_index=True, related_name='+')
     target = models.ForeignKey(settings.OAUTH2_PROVIDER_APPLICATION_MODEL,
                                db_index=True, related_name='+')
+
+    def __str__(self):
+        return "%s -> %s" % (self.requester, self.target)
