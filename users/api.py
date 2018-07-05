67e27f2263d14edad61be1ec90b71063e4bd66eb
Tuomas Haapala <tuomas.haapala@anders.fi>
2018-07-05 13:11:45 +0300

    Add user consent API endpoint

--- users/api.py
+++ users/api.py
@@ -1,3 +1,4 @@
+from oidc_provider.models import UserConsent
 from rest_framework import mixins, serializers, viewsets
 from rest_framework.permissions import IsAuthenticated
 
@@ -22,3 +23,31 @@ class UserLoginEntryViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
 
     def get_queryset(self):
         return self.queryset.filter(user_id=self.request.user.id)
+
+
+class UserConsentSerializer(serializers.ModelSerializer):
+    service = serializers.SerializerMethodField()
+    scopes = serializers.SerializerMethodField()
+
+    class Meta:
+        model = UserConsent
+        fields = ('id', 'date_given', 'expires_at', 'service', 'scopes')
+
+    def get_service(self, obj):
+        return obj.client.service.id
+
+    def get_scopes(self, obj):
+        return [s for s in obj.scope if s != 'openid']
+
+
+class UserConsentViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin, mixins.DestroyModelMixin,
+                         viewsets.GenericViewSet):
+    serializer_class = UserConsentSerializer
+    queryset = UserConsent.objects.select_related('client__service')
+    pagination_class = DefaultPagination
+    authentication_classes = (OidcTokenAuthentication,)
+    permission_classes = (IsAuthenticated, ScopePermission)
+    required_scopes = ('consents',)
+
+    def get_queryset(self):
+        return self.queryset.filter(user_id=self.request.user.id).exclude(client__service=None)
