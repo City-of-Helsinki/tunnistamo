66a9d140feb3a0bd332031853fb1038622fd5c5b
Timo Tuominen <tituomin@gmail.com>
2018-11-10 16:55:22 +0200

    Implement current session auth method check

--- oidc_apis/utils.py
+++ oidc_apis/utils.py
@@ -1,4 +1,10 @@
 from collections import OrderedDict
+import django
+from oidc_provider import settings
+from django.contrib.auth import BACKEND_SESSION_KEY
+from django.contrib.auth import logout as django_user_logout
+from users.models import LoginMethod, OidcClientOptions
+from django.contrib.auth.views import redirect_to_login
 
 
 def combine_uniquely(iterable1, iterable2):
@@ -22,6 +28,23 @@ def after_userlogin_hook(request, user, client):
     oidc-provider fresh. This is achieved by pointing
     'OIDC_AFTER_USERLOGIN_HOOK' setting to this."""
     request.session.modified = True
+
+    last_login_backend = request.session.get('social_auth_last_login_backend')
+    client_options = OidcClientOptions.objects.get(oidc_client=client)
+
+    allowed_methods = client_options.login_methods.all()
+    if allowed_methods is None:
+        raise django.core.exceptions.PermissionDenied
+
+    allowed_providers = set((x.provider_id for x in allowed_methods))
+    if last_login_backend is not None:
+        active_backend = user.social_auth.filter(provider=last_login_backend)
+
+    if ((last_login_backend is None and user is not None)
+            or (active_backend.exists() and active_backend.first().provider not in allowed_providers)):
+        django_user_logout(request)
+        next_page = request.get_full_path()
+        return redirect_to_login(next_page, settings.get('OIDC_LOGIN_URL'))
 
     # Return None to continue the login flow
     return None
