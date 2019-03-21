7d6de77fe177d5560c5b265a0c85dab6ead8a9b5
Timo Tuominen <tituomin@gmail.com>
2019-03-21 11:29:38 +0200

    Fix previous faulty commit

--- tunnistamo/error_handlers.py
+++ tunnistamo/error_handlers.py
@@ -7,9 +7,9 @@ class TunnistamoSocialAuthExceptionMiddleware(SocialAuthExceptionMiddleware):
     # Override get_redirect_uri to point the user back to backend selection
     # instead of non-functioning login page in case of authentication error
     def get_redirect_uri(self, request, exception):
+        strategy = getattr(request, 'social_strategy', None)
         if strategy.session.get('next') is None:
             return super().get_redirect_uri(self, request, exception)
-        strategy = getattr(request, 'social_strategy', None)
         url = '/login/?next=' + quote(strategy.session.get('next'))
         return url
 
