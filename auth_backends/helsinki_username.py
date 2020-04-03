b7ffabac6dae62bc568b2421056ccbf91f0e4577
Ville Koivunen <ville.koivunen@hel.fi>
2020-04-03 17:00:32 +0300

    Add a stand-in background color and make OIDC_ENDPOINT configurable

--- auth_backends/helsinki_username.py
+++ auth_backends/helsinki_username.py
@@ -3,10 +3,12 @@ from social_core.backends.open_id_connect import OpenIdConnectAuth
 
 class HelsinkiUsername(OpenIdConnectAuth):
     """Uses the Helsinki username/email & password service for
-       authenticating the user"""
+       authenticating the user. Currently this is bog-standard OIDC"""
     name = 'helusername'
 
-    OIDC_ENDPOINT = "https://salasana.hel.ninja/auth/realms/helsinki-salasana"
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.OIDC_ENDPOINT = self.setting('OIDC_ENDPOINT')
 
     def validate_and_return_id_token(self, id_token, access_token):
         super().validate_and_return_id_token(id_token, None)
