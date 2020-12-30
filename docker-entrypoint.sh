74d0dca7a78251656222c19b2c35a580431d4d95
mskoskel <76683218+mskoskel@users.noreply.github.com>
2020-12-30 20:44:47 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,14 +2,14 @@
 
 set -e
 
-if [ -z "$SKIP_DATABASE_CHECK" -o "$SKIP_DATABASE_CHECK" = "0" ]; then
-  until nc -z -v -w30 "$DATABASE_HOST" 5432
-  do
-    echo "Waiting for postgres database connection..."
-    sleep 1
-  done
-  echo "Database is up!"
-fi
+# if [ -z "$SKIP_DATABASE_CHECK" -o "$SKIP_DATABASE_CHECK" = "0" ]; then
+# until nc -z -v -w30 "$DATABASE_HOST" 5432
+#  do
+#   echo "Waiting for postgres database connection..."
+#    sleep 1
+#  done
+#  echo "Database is up!"
+# fi
 
 # Apply database migrations
 if [[ "$APPLY_MIGRATIONS" = "1" ]]; then
