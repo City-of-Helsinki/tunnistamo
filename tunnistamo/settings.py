7ca95cba1e873cb903c9acc19624e1e9721f95e3
Timo Tuominen <tituomin@gmail.com>
2021-02-03 17:45:46 +0200

    HP-388 Use more specific exception class (sonar)

--- tunnistamo/settings.py
+++ tunnistamo/settings.py
@@ -720,4 +720,5 @@ if 'SECRET_KEY' not in locals():
                     f.write(SECRET_KEY)
                     f.close()
             except IOError:
-                raise Exception('Please create a %s file with random characters to generate your secret key!' % secret_file)
+                raise IOError(
+                    'Please create a %s file with random characters to generate your secret key!' % secret_file)
