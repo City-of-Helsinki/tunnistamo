3817bb2c87501ddbc4f77e124a070524320c3a45
Mikko Keskinen <mikko.keskinen@hel.fi>
2021-05-24 14:25:58 +0300

    Change get_loa to use only extra_data
    The id_token is saved to the instance attribute only temporarily so no reason to use it at all.

--- auth_backends/tests/test_helsinki_tunnistus_suomifi.py
+++ auth_backends/tests/test_helsinki_tunnistus_suomifi.py
@@ -3,30 +3,16 @@ import pytest
 from auth_backends.helsinki_tunnistus_suomifi import HelsinkiTunnistus
 
 
-def test_get_loa_low_without_id_token():
+def test_get_loa_low_without_social_auth():
     backend = HelsinkiTunnistus()
 
-    assert backend.get_loa() == 'low'
-
-
-@pytest.mark.parametrize('id_token_loa_value', (None, 'low', 'substantial'))
-def test_get_loa_from_id_token(id_token_loa_value):
-    backend = HelsinkiTunnistus()
-    backend.id_token = {}
-
-    if id_token_loa_value:
-        backend.id_token['loa'] = id_token_loa_value
-
-    expected_loa_value = id_token_loa_value if id_token_loa_value else 'low'
-
-    assert backend.get_loa() == expected_loa_value
+    assert backend.get_loa(None) == 'low'
 
 
 @pytest.mark.django_db
 @pytest.mark.parametrize('id_token_loa_value', (None, 'low', 'substantial'))
-def test_get_loa_from_social_auth_extra_data(user_factory, usersocialauth_factory, id_token_loa_value):
+def test_get_loa(user_factory, usersocialauth_factory, id_token_loa_value):
     backend = HelsinkiTunnistus()
-    backend.id_token = None
 
     user = user_factory()
     social_auth = usersocialauth_factory(provider=backend.name, user=user)
