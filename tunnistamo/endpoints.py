34599ae2e381a7822d3aaa6b7dda90ee8da07869
Mikko Keskinen <mikko.keskinen@hel.fi>
2021-08-19 16:51:28 +0300

    HP-592 Add Tunnistamo Session validity checks to OIDC provider views
    Session is valid if session exists and hasn't ended.
    
    Also change a couple of tests to include a suitable Tunnistamo Session.

--- tunnistamo/endpoints.py
+++ tunnistamo/endpoints.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.utils.module_loading import import_string
 from jwkest.jwt import JWT
 from oidc_provider.lib.endpoints.authorize import AuthorizeEndpoint
+from oidc_provider.lib.endpoints.introspection import TokenIntrospectionEndpoint
 from oidc_provider.lib.endpoints.token import TokenEndpoint
+from oidc_provider.lib.errors import AuthorizeError, TokenError, TokenIntrospectionError
 
 from services.models import Service
 from users.models import TunnistamoSession, UserLoginEntry
@@ -16,7 +18,19 @@ def _create_userloginentry_for_client(request, client):
         pass
 
 
-class TunnistamoAuthorizeEndpoint(AuthorizeEndpoint):
+class TunnistamoSessionEndpointMixin:
+    def _get_tunnistamo_session(self):
+        raise NotImplementedError('Implement in subclass')
+
+    def is_tunnistamo_session_active(self):
+        tunnistamo_session = self._get_tunnistamo_session()
+        if not tunnistamo_session or tunnistamo_session.has_ended():
+            return False
+
+        return True
+
+
+class TunnistamoAuthorizeEndpoint(TunnistamoSessionEndpointMixin, AuthorizeEndpoint):
     def _get_tunnistamo_session(self):
         tunnistamo_session_id = self.request.session.get("tunnistamo_session_id")
         if not tunnistamo_session_id:
@@ -26,6 +40,12 @@ class TunnistamoAuthorizeEndpoint(AuthorizeEndpoint):
             return TunnistamoSession.objects.get(pk=tunnistamo_session_id)
         except TunnistamoSession.DoesNotExist:
             return None
+
+    def validate_params(self):
+        super().validate_params()
+
+        if self.request.user.is_authenticated and not self.is_tunnistamo_session_active():
+            raise AuthorizeError(self.params['redirect_uri'], 'access_denied', self.grant_type)
 
     def create_code(self):
         code = super().create_code()
@@ -57,12 +77,18 @@ class TunnistamoAuthorizeEndpoint(AuthorizeEndpoint):
         )
 
 
-class TunnistamoTokenEndpoint(TokenEndpoint):
+class TunnistamoTokenEndpoint(TunnistamoSessionEndpointMixin, TokenEndpoint):
     def _get_tunnistamo_session(self):
         if hasattr(self, 'code') and self.code:
             return TunnistamoSession.objects.get_by_element(self.code)
         elif hasattr(self, 'token') and self.token:
             return TunnistamoSession.objects.get_by_element(self.token)
+
+    def validate_params(self):
+        super().validate_params()
+
+        if not self.is_tunnistamo_session_active():
+            raise TokenError('Session not found or expired')
 
     def create_token(self, user, client, scope):
         token = super().create_token(user, client, scope)
@@ -97,3 +123,15 @@ class TunnistamoTokenEndpoint(TokenEndpoint):
                 pass
 
         return dic
+
+
+class TunnistamoTokenIntrospectionEndpoint(TunnistamoSessionEndpointMixin, TokenIntrospectionEndpoint):
+    def _get_tunnistamo_session(self):
+        if hasattr(self, 'token') and self.token:
+            return TunnistamoSession.objects.get_by_element(self.token)
+
+    def validate_params(self):
+        super().validate_params()
+
+        if not self.is_tunnistamo_session_active():
+            raise TokenIntrospectionError('Session not found or expired')
