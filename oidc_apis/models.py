2b3c31ba0f0ab46f9ff1411a627e2c84883aa289
Mikko Keskinen <mikko.keskinen@hel.fi>
2021-10-21 14:49:01 +0300

    HP-1040 Change from deprecated ugettext_lazy to gettext_lazy
    ugettext_lazy has been an alias to gettext_lazy from Django 2.0 and will be removed in Django 4.0

--- oidc_apis/models.py
+++ oidc_apis/models.py
@@ -2,7 +2,7 @@ from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 from django.db import models
 from django.utils.crypto import get_random_string
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from multiselectfield import MultiSelectField
 from oidc_provider.models import Client
 from parler.fields import TranslatedField
--- services/models.py
+++ services/models.py
@@ -1,6 +1,6 @@
 from django.core.exceptions import ValidationError
 from django.db import models
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 from oidc_provider.models import Client
 from parler.models import TranslatableModel, TranslatedFields
 
