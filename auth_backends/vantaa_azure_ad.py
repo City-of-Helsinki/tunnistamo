6700923c52678e0be306eab525673c66c7ab6934
Mikko Keskinen <mikko.keskinen@hel.fi>
2023-02-10 14:21:26 +0200

    Add new base class for Azure AD backends
    The common requirement for Azure AD backends is to set "ad_groups" key
    into the details from the "groups" key in the response.

--- auth_backends/vantaa_azure_ad.py
+++ auth_backends/vantaa_azure_ad.py
@@ -1,11 +1,5 @@
-from social_core.backends.azuread_tenant import AzureADV2TenantOAuth2
+from auth_backends.azure_ad import AzureADV2TenantOAuth2WithADGroups
 
 
-class VantaaAzureADV2TenantOAuth2(AzureADV2TenantOAuth2):
+class VantaaAzureADV2TenantOAuth2(AzureADV2TenantOAuth2WithADGroups):
     name = 'vantaaazuread'
-
-    def get_user_details(self, response):
-        details = super().get_user_details(response)
-        details['ad_groups'] = response.get('groups')
-
-        return details
