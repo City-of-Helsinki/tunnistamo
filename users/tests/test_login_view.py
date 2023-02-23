1552ce5a83d9d2382f0c19953a9ddeda55666616
Mikko Keskinen <mikko.keskinen@hel.fi>
2023-02-23 12:59:56 +0200

    HP-1732 Add ability to filter login methods with an "idp_hint"-parameter
    The parameter filters only the login methods that are used when the
    login is initiated by the OIDC or OAuth2 flows.
    
    Unknown provider ids are effectively skipped and all clients' allowed
    login methods are shown if no provider ids match.
    
    The "idp_hint"-parameter is relayed to the social auth begin-view so
    that the parameter can be kept intact if the social auth redirects the
    user back to the login view.

--- users/tests/test_login_view.py
+++ users/tests/test_login_view.py
@@ -138,3 +138,122 @@ def test_login_view_loginmethods_per_oidcclient(client, assertCountEqual, loginm
     response = client.get('/login/', params)
 
     assertCountEqual(response.context['login_methods'], login_methods)
+
+
+@pytest.fixture
+def oidc_client(
+    loginmethod_factory, oidcclient_factory, oidcclientoptions_factory
+):
+    oidc_client = oidcclient_factory(redirect_uris=['https://example.com/'])
+    oidc_client_options = oidcclientoptions_factory(oidc_client=oidc_client)
+    oidc_client_options.login_methods.set([
+        loginmethod_factory(provider_id='facebook'),
+        loginmethod_factory(provider_id='github'),
+        loginmethod_factory(provider_id='google'),
+    ]),
+
+    return oidc_client
+
+
+@pytest.mark.django_db
+def test_login_view_one_valid_idp_hint_should_redirect(client, oidc_client):
+    login_method = oidc_client.options.login_methods.first()
+
+    params = {
+        'next': f'/openid/authorize?client_id={oidc_client.client_id}',
+        'idp_hint': login_method.provider_id,
+    }
+
+    response = client.get('/login/', params)
+
+    assert response.status_code == 302
+    assert response['Location'].startswith(
+        reverse('social:begin', kwargs={'backend': login_method.provider_id})
+    )
+
+
+@pytest.mark.django_db
+def test_login_view_two_valid_idp_hints(
+    client, oidc_client, assertCountEqual
+):
+    login_methods = oidc_client.options.login_methods.all()[:2]
+
+    params = {
+        'next': f'/openid/authorize?client_id={oidc_client.client_id}',
+        'idp_hint': ','.join([login_method.provider_id for login_method in login_methods]),
+    }
+
+    response = client.get('/login/', params)
+
+    assertCountEqual(response.context['login_methods'], login_methods)
+
+
+@pytest.mark.django_db
+def test_login_view_not_allowed_provider_in_idp_hint_should_not_be_included(
+    client, oidc_client, loginmethod_factory, assertCountEqual
+):
+    login_methods = list(oidc_client.options.login_methods.all())
+    not_allowed_login_method = loginmethod_factory(provider_id='helsinki_adfs')
+
+    params = {
+        'next': f'/openid/authorize?client_id={oidc_client.client_id}',
+        'idp_hint': ','.join([
+            login_method.provider_id for login_method in login_methods + [not_allowed_login_method]
+        ]),
+    }
+
+    response = client.get('/login/', params)
+
+    assertCountEqual(response.context['login_methods'], login_methods)
+
+
+@pytest.mark.django_db
+def test_login_view_unknown_provider_in_idp_hint_should_not_be_included_and_no_error(
+    client, oidc_client, loginmethod_factory, assertCountEqual
+):
+    login_methods = oidc_client.options.login_methods.all()
+
+    idp_hint = ','.join([login_method.provider_id for login_method in login_methods])
+    idp_hint += ',unknown_provider_id'
+
+    params = {
+        'next': f'/openid/authorize?client_id={oidc_client.client_id}',
+        'idp_hint': idp_hint,
+    }
+
+    response = client.get('/login/', params)
+
+    assertCountEqual(response.context['login_methods'], login_methods)
+
+
+@pytest.mark.django_db
+def test_login_view_single_non_allowed_provider_in_idp_hint_should_show_all_login_methods_in_client(
+    client, oidc_client, loginmethod_factory, assertCountEqual
+):
+    login_methods = oidc_client.options.login_methods.all()
+    not_allowed_login_method = loginmethod_factory(provider_id='helsinki_adfs')
+
+    params = {
+        'next': f'/openid/authorize?client_id={oidc_client.client_id}',
+        'idp_hint': not_allowed_login_method.provider_id,
+    }
+
+    response = client.get('/login/', params)
+
+    assertCountEqual(response.context['login_methods'], login_methods)
+
+
+@pytest.mark.django_db
+def test_login_view_unknown_idp_hint_should_show_all_login_methods_in_client(
+    client, oidc_client, assertCountEqual
+):
+    login_methods = oidc_client.options.login_methods.all()
+
+    params = {
+        'next': f'/openid/authorize?client_id={oidc_client.client_id}',
+        'idp_hint': 'unknown_provider_id',
+    }
+
+    response = client.get('/login/', params)
+
+    assertCountEqual(response.context['login_methods'], login_methods)
